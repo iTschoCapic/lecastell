893ae15fc3a67db6c2b933acf2172d6a6057cfd0
iTs_choCapic <52358452+iTschoCapic@users.noreply.github.com>
2024-07-21 03:28:40 +0200

    added translation for spanish and catalan, added animation for footer, added first phone version

--- animations.js
+++ animations.js
@@ -1,11 +1,27 @@
+/* ------------------------------------------------------------- */
+/* ------------------- Animation Text-Header ------------------- */
+/* ------------------------------------------------------------- */
+
+window.onload = function () {
+    console.log("index");
+    if (window.location.pathname == '/'){
+        var textH = document.getElementById('text-header');
+        textH.style.animation = "slideInTextHeader 0.5s forwards";
+        console.log("index2");
+    }
+}
+/* ------------------------------------------------------------- */
+/* --------------------- Animation Language --------------------- */
+/* ------------------------------------------------------------- */
+
 function toggleMenuLanguage() {
     var sublanguage = document.getElementById('sublanguage');    
     if(sublanguage.style.display == "flex") { // if is sublanguage displayed, hide it
-        sublanguage.style.animation = "slideOut 0.5s forwards"
+        sublanguage.style.animation = "slideOutLanguage 0.5s forwards";
         setTimeout(() => {  sublanguage.style.display = "none"; }, 500);
     } else { // if is sublanguage hidden, display it
         sublanguage.style.display = "flex";
-        sublanguage.style.animation = "slideIn 0.5s forwards"
+        sublanguage.style.animation = "slideInLanguage 0.5s forwards";
     }
 }
   
@@ -17,10 +33,24 @@ window.onclick = function(event) {
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.style.display == "flex") {
-                sublanguage.style.animation = "slideOut 0.5s forwards"
+                sublanguage.style.animation = "slideOutLanguage 0.5s forwards";
                 setTimeout(() => {  sublanguage.style.display = "none"; }, 500);
                 
             }
         }
     }
-} 
+}
+
+/* ------------------------------------------------------------- */
+/* ------------------ Animation Arrow Footer ------------------ */
+/* ------------------------------------------------------------- */
+
+function displayFooter() {
+    var footer = document.getElementById('footer');
+    if(footer.style.display == "flex") { // if is arrowFooter displayed, hide it
+        footer.style.display = "none";
+    } else { // if is arrowFooter hidden, display it
+        footer.style.display = "flex";
+        footer.scrollIntoView({ behavior: 'smooth' });
+    }
+}
