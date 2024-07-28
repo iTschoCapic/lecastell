2409e8e263d2535cfd37cfd79ef6c4ec94e38bdf
iTs_choCapic <52358452+iTschoCapic@users.noreply.github.com>
2024-07-28 17:23:15 +0200

    Link to Menus.html remove for phone, modified behaviours of animation (modal, menu, sublanguage), correction of hitbox for logo in phone version

--- animations.js
+++ animations.js
@@ -1,3 +1,6 @@
+var Modal = false;
+var SubLanguage = false;
+
 /* ------------------------------------------------------------- */
 /* ------------------- Animation Text-Header ------------------- */
 /* ------------------------------------------------------------- */
@@ -10,6 +13,18 @@ window.onload = function () {
         console.log('index2');
     }
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuLink = document.getElementById('menu_link');
+    menuLink.addEventListener('click', function (event) {
+        if (window.innerWidth <= 768) {
+            event.preventDefault();  // Prevent the link from being followed
+            this.parentElement.classList.toggle('focus-within');  // Toggle the display of the submenu
+        }
+    });
+});
+
 /* ------------------------------------------------------------- */
 /* --------------------- Animation Language --------------------- */
 /* ------------------------------------------------------------- */
@@ -17,27 +32,13 @@ window.onload = function () {
 function toggleMenuLanguage() {
     var sublanguage = document.getElementById('sublanguage');
     if(sublanguage.style.display == 'flex') { // if is sublanguage displayed, hide it
+        SubLanguage = false;
         sublanguage.style.animation = 'slideOutLanguage 0.5s forwards';
         setTimeout(() => {  sublanguage.style.display = 'none'; }, 500);
     } else { // if is sublanguage hidden, display it
+        SubLanguage = true;
         sublanguage.style.display = 'flex';
         sublanguage.style.animation = 'slideInLanguage 0.5s forwards';
-    }
-}
-  
-  // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    var dropdowns = document.getElementById('sublanguage');
-    if (!event.target.matches('.languageIMG') && dropdowns.style.display != 'none') {
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.style.display == 'flex') {
-                sublanguage.style.animation = 'slideOutLanguage 0.5s forwards';
-                setTimeout(() => {  sublanguage.style.display = 'none'; }, 500);
-                
-            }
-        }
     }
 }
 
@@ -45,18 +46,33 @@ window.onclick = function(event) {
 /* ----------------- Animation Fullscreen Card ----------------- */
 /* ------------------------------------------------------------- */
 
+document.addEventListener('DOMContentLoaded', function () {
+    var card = document.getElementById('card');
+    var modal = document.getElementById('cardFull');
+    card.addEventListener('click', function (event) {
+        if (window.innerWidth <= 768) {
+            if(event.target.matches('.card')){
+                modal.style.display = 'block';
+                Modal = true;
+            } else if (event.target.matches('.close') || !event.target.matches('.card')) {
+                modal.style.display = 'none';
+            }
+        }
+    });
+});
 
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+/* ------------------------------------------------------------- */
+/* ----------------- General Rule for closing ----------------- */
+/* ------------------------------------------------------------- */
 
 window.onclick = function(event){
     if (window.innerWidth <= 768){
-        var modal = document.getElementById('cardFull');
-        if(event.target.matches('.card')){
-            modal.style.display = 'block';
-        } else if (event.target.matches('.close') || !event.target.matches('.card')) {
-            modal.style.display = 'none';
+        if (Modal && (event.target.matches('.close') || !event.target.matches('.card'))){
+            Modal = false;
+            document.getElementById('cardFull').style.display = 'none';
+        } else if (SubLanguage && !event.target.matches('.languageIMG')) {
+            SubLanguage = false;
+            toggleMenuLanguage();
         }
     }
 }
-
