93dd434a4c31934b8c811cf395f8bb703d7125f5
iTs_choCapic <52358452+iTschoCapic@users.noreply.github.com>
2024-07-28 15:49:58 +0200

    added contact, icon, phone version : index, cgu, contact, modification of footer, modal images for cards, ordering menu, added allergernes stickers

--- animations.js
+++ animations.js
@@ -3,11 +3,11 @@
 /* ------------------------------------------------------------- */
 
 window.onload = function () {
-    console.log("index");
+    console.log('index');
     if (window.location.pathname == '/'){
         var textH = document.getElementById('text-header');
-        textH.style.animation = "slideInTextHeader 0.5s forwards";
-        console.log("index2");
+        textH.style.animation = 'slideInTextHeader 0.5s forwards';
+        console.log('index2');
     }
 }
 /* ------------------------------------------------------------- */
@@ -15,26 +15,26 @@ window.onload = function () {
 /* ------------------------------------------------------------- */
 
 function toggleMenuLanguage() {
-    var sublanguage = document.getElementById('sublanguage');    
-    if(sublanguage.style.display == "flex") { // if is sublanguage displayed, hide it
-        sublanguage.style.animation = "slideOutLanguage 0.5s forwards";
-        setTimeout(() => {  sublanguage.style.display = "none"; }, 500);
+    var sublanguage = document.getElementById('sublanguage');
+    if(sublanguage.style.display == 'flex') { // if is sublanguage displayed, hide it
+        sublanguage.style.animation = 'slideOutLanguage 0.5s forwards';
+        setTimeout(() => {  sublanguage.style.display = 'none'; }, 500);
     } else { // if is sublanguage hidden, display it
-        sublanguage.style.display = "flex";
-        sublanguage.style.animation = "slideInLanguage 0.5s forwards";
+        sublanguage.style.display = 'flex';
+        sublanguage.style.animation = 'slideInLanguage 0.5s forwards';
     }
 }
   
   // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.languageIMG')) {
-        var dropdowns = document.getElementsByClassName("sublanguage");
+    var dropdowns = document.getElementById('sublanguage');
+    if (!event.target.matches('.languageIMG') && dropdowns.style.display != 'none') {
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.style.display == "flex") {
-                sublanguage.style.animation = "slideOutLanguage 0.5s forwards";
-                setTimeout(() => {  sublanguage.style.display = "none"; }, 500);
+            if (openDropdown.style.display == 'flex') {
+                sublanguage.style.animation = 'slideOutLanguage 0.5s forwards';
+                setTimeout(() => {  sublanguage.style.display = 'none'; }, 500);
                 
             }
         }
@@ -42,15 +42,21 @@ window.onclick = function(event) {
 }
 
 /* ------------------------------------------------------------- */
-/* ------------------ Animation Arrow Footer ------------------ */
+/* ----------------- Animation Fullscreen Card ----------------- */
 /* ------------------------------------------------------------- */
 
-function displayFooter() {
-    var footer = document.getElementById('footer');
-    if(footer.style.display == "flex") { // if is arrowFooter displayed, hide it
-        footer.style.display = "none";
-    } else { // if is arrowFooter hidden, display it
-        footer.style.display = "flex";
-        footer.scrollIntoView({ behavior: 'smooth' });
+
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+window.onclick = function(event){
+    if (window.innerWidth <= 768){
+        var modal = document.getElementById('cardFull');
+        if(event.target.matches('.card')){
+            modal.style.display = 'block';
+        } else if (event.target.matches('.close') || !event.target.matches('.card')) {
+            modal.style.display = 'none';
+        }
     }
 }
+
